0c25093cbb31075b1d50262adfc0432dbfebdf48
oliflox <83953551+oliflox@users.noreply.github.com>
2024-05-20 15:59:13 +0200

    Close button

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,12 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+const closeModal = document.querySelector('.close');
+
+const form = document.getElementById('myForm');
+const firstField = document.getElementById('first');
+const lastField = document.getElementById('last');
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -20,4 +26,7 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// Close modal event
+closeModal.addEventListener('click', function() {
+  modalbg.style.display = "none"; 
+});
