b264977debba85be1fd390bf9750ef2f06fd69c6
oliflox <83953551+oliflox@users.noreply.github.com>
2024-05-27 19:04:14 +0200

    Error message

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -24,7 +24,14 @@ const userConditions = document.getElementById('checkbox1');
 
 const radioButtons = document.querySelectorAll('input[name="location"]');
 
-const successMessage = document.getElementById('successMessage');
+
+const firstError = document.getElementById('firstError');
+const lastError = document.getElementById('lastError');
+const emailError = document.getElementById('emailError');
+const quantityError = document.getElementById('quantityError');
+const locationError = document.getElementById('locationError');
+const userConditionsError = document.getElementById('userConditionsError');
+
 
 // Regular expression for email validation
 const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
@@ -67,44 +74,94 @@ document.addEventListener('DOMContentLoaded', (event) => {
   form.noValidate = true;
 });
 
+function firstFielderror() {
+  if (
+    firstField.value.trim() === "" || firstField.value.trim().length < 2) {
+    firstField.style.border = "2px solid red";
+    firstError.style.display = "block";
+    return false; 
+  }
+  else {
+    firstField.style.border = "none";
+    firstError.style.display = "none";
+    return true ;
+  }
+}
+
+function lastFielderror() {
+  if (
+    lastField.value.trim() === "" || lastField.value.trim().length < 2) {
+    lastField.style.border = "2px solid red";
+    lastError.style.display = "block";
+    return false;
+  }
+  else {
+    lastField.style.border = "none";
+    lastError.style.display = "none";
+    return true ;
+  }
+}
+
+function emailFielderror() {
+  if (!emailField.value.match(emailRegex)) {
+    emailField.style.border = "2px solid red";
+    emailError.style.display = "block";
+    return false;
+  }
+  else {
+    emailField.style.border = "none";
+    emailError.style.display = "none";
+    return true ;
+  }
+}
+
+function quantityFielderror() {
+  if (isNaN(quantityField.value) || quantityField.value.trim() === "" || quantityField.value < 0 || quantityField.value > 99) {
+    quantityField.style.border = "2px solid red";
+    quantityError.style.display = "block";
+    return false;
+  }
+  else {
+    quantityField.style.border = "none";
+    quantityError.style.display = "none";
+    return true ;
+  }
+}
+
+function userConditionserror() {
+  if (!userConditions.checked) {
+    userConditionsError.style.display = "block";
+    return false;
+  }
+  else {
+    userConditionsError.style.display = "none";
+    return true ;
+  }
+}
+
+function radioButtonserror() {
+  if (!isRadioChecked()) {
+    locationError.style.display = "block";
+
+    return false;
+  }
+  else {
+    locationError.style.display = "none";
+    return true ;
+  }
+}
+
 // Form validation
 function validate() {
 
-  if (firstField.value.trim() === "" || firstField.value.trim().length < 2 ||
-    lastField.value.trim() === "" || lastField.value.trim().length < 2 ||
-    !emailField.value.match(emailRegex) ||
-    isNaN(quantityField.value) || quantityField.value.trim() === "" || quantityField.value < 0 || quantityField.value > 99 ||
-    !userConditions.checked || 
-    !isRadioChecked()) {
+ if (!firstFielderror() || !lastFielderror() || !emailFielderror() || !quantityFielderror() || !userConditionserror() || !radioButtonserror()) {
     return false;
-  } else {
-    successMessage.style.display = "block";
+}
+  else {
     modalbg.style.display = "none";
+    document.body.classList.remove('modal-open');
     return true;
   }
-
-  // let isValid = true;
-
-  // if(){
-  //   isValid = false;
-  // }
-
-  // if(){
-  //   isValid = false;
-  // }
-
-  // if(){
-  //   isValid = false;
-  // }
-
-  // if(){
-  //   isValid = false;
-  // }
-
-  // if(){
-  //   isValid = false;
-  // }
-  // return isValid;
 }
 
 
